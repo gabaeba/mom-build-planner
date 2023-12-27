8c024d2df9fc24caaf08368e745f91717c4d7bac
Gaba Gaba <gabaeba@gmail.com>
2023-12-27 00:34:11 -0300

    testing

--- src/pages/thief/assassin-cross/assassin-cross.tsx
+++ src/pages/thief/assassin-cross/assassin-cross.tsx
@@ -1,7 +1,3 @@
-import React from 'react'
-
 export const AssassinCross = () => {
-  return (
-    <div>assassin-cross</div>
-  )
-}
+  return <div>assassin-cross</div>;
+};
--- src/pages/thief/stalker/stalker.tsx
+++ src/pages/thief/stalker/stalker.tsx
@@ -1,7 +1,3 @@
-import React from 'react'
-
 export const Stalker = () => {
-  return (
-    <div>stalker</div>
-  )
-}
+  return <div>stalker</div>;
+};
