c9b98d809f34df6d2d59a41d35868d4e00a376ef
Gaba Gaba <gabaeba@gmail.com>
2023-12-31 09:49:03 -0300

    testing hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { QueryParamProvider } from "use-query-params";
 import { ReactRouter5Adapter } from "use-query-params/adapters/react-router-5";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/mom-build-planner">
+    <HashRouter basename="/mom-build-planner">
       <QueryParamProvider adapter={ReactRouter5Adapter}>
         <App />
       </QueryParamProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
